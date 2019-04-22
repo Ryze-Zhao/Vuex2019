6059cab1203c6370f7d26a8084e65b08f8abef0a
RyzeZhao <643080112@qq.com>
2019-04-22 13:17:02 +0800

    Lesson02：2.2    包管理器（npm，使用cmd写命令）

---
+++ Lesson02/MyPackageDemo/src/index.js
@@ -0,0 +1,23 @@
+
+import 'es6-promise/auto'
+import Vue from 'vue'
+import Vuex from 'vuex'
+// const Vue=require('vue')
+// const Vuex=require('vuex')
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment (state) {
+        state.count++
+        }
+    }
+})
+
+console.log(store.state.count)
+store.commit("increment")
+console.log(store.state.count)
