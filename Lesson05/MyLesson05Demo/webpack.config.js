db40c2218fda2011b5d5f6ab5ec7aea7ae217717
RyzeZhao <643080112@qq.com>
2019-04-23 13:00:36 +0800

    5.9    ​解决冲突，可以再webpack.config.js中指定使用的vue版本，具体版本可以从引入的node_modules/vue/dist中看js文件名字写入，如图

--- Lesson05/MyLesson05Demo/webpack.config.js
+++ Lesson05/MyLesson05Demo/webpack.config.js
@@ -3,7 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     mode:'development', // 开发环境
     entry:"./src/index.js", //入口文件
- 
+    resolve: {
+        alias: {
+            "vue": "vue/dist/vue.esm.js"
+        }
+    },
     plugins:[
         //引入插件（const HtmlWebpackPlugin = require("html-webpack-plugin")）,并需要生产的html文件
         new HtmlWebpackPlugin({
