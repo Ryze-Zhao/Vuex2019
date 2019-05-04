8d1e1f9dcc58e62a224356e34407be0b832bfe9f
RyzeZhao <643080112@qq.com>
2019-05-04 22:02:29 +0800

    12.13    ​main.js的引用代码，我们已经在12.3改好了。

--- Lesson12/MyCode/src/main.js
+++ Lesson12/MyCode/src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import store from './store';
-//1、引入App.vue
 import App from './components/App.vue';
 
-//2、使用App.vue
 const app = new Vue({
     el:'#app',
     store,
