d6e2d09bfbd195a54b3670578d1de4c18d011ee7
RyzeZhao <643080112@qq.com>
2019-05-04 21:45:09 +0800

    12.7    ​开始引入其他模块ProductList、ShoppingCart，先修改App.vue

--- Lesson12/MyCode/src/components/App.vue
+++ Lesson12/MyCode/src/components/App.vue
@@ -1,9 +1,25 @@
 <template>
-    <div>
-        Hallo
-    </div>
+<!-- 3、使用 -->
+<div id="app">
+        <h1>Shopping Cart Example</h1>
+        <hr>
+        <h2>Products</h2>
+        <ProductList/>
+        <hr>
+        <ShoppingCart/>
+  </div>
 </template>
 <script>
+//1、引入ProductList、ShoppingCart
+import ProductList from './ProductList.vue'
+import ShoppingCart from './ShoppingCart.vue'
+export default {
+  //2、装进来，使用
+    components:{
+        ProductList,
+        ShoppingCart,
+    }
+}
 </script>
 <style>
 </style>
