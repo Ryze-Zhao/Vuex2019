41c0efda40d876af7df20fd7200ece941df21e85
RyzeZhao <643080112@qq.com>
2019-05-06 21:41:14 +0800

    14.3.1    ​修改demo02/src/store/index.js

--- Lesson14/MyCode/demo02/src/store/index.js
+++ Lesson14/MyCode/demo02/src/store/index.js
@@ -1,16 +1,18 @@
 import Vuex from 'vuex';
-//1、引入Logger
 import createLogger from 'vuex/dist/logger';
-//2、引入插件
-export default function createStore(){
-   return new Vuex.Store({
-        state:{
-            count:1,
+
+console.log("NODE_ENV:",process.env.NODE_ENV);
+export default function createStore() {
+    return new Vuex.Store({
+        //非生产环境使用严格模式
+        strict: process.env.NODE_ENV !== 'production',
+        state: {
+            count: 1,
         },
         mutations: {
-            increment (state) {
+            increment(state) {
                 state.count++
             }
-        },  plugins: [createLogger()]
-    }) 
+        }, plugins: [createLogger()]
+    })
 }
