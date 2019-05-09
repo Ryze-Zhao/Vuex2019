1205beaf2c00d3058425b096ad6773eca204b224
RyzeZhao <643080112@qq.com>
2019-05-09 10:05:59 +0800

    16.1.4    index.js中引入mutations

--- Lesson16/MyCode/demo01/src/store/index.js
+++ Lesson16/MyCode/demo01/src/store/index.js
@@ -1,12 +1,13 @@
 import Vuex from 'vuex';
 import createLogger from 'vuex/dist/logger';
-
-
+//1、导入
+import { mutations } from './mutations';
 
 export default function createStore(){
    return new Vuex.Store({
-        state:{
-            count:1,
-        },  plugins: [createLogger()]
+        state:{count:1},
+        //2、引入
+        mutations,
+        plugins: [createLogger()]
     }) 
 }
